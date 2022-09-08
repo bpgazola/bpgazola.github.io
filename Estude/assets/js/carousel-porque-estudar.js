d1913a88f476c87ffb01f2eeba414f7b65471729
Bruno Gazola de Paoli <bpgazola@hotmail.com>
2022-09-08 11:59:57 -0300

    Create carousel-porque-estudar.js

---
+++ Estude/assets/js/carousel-porque-estudar.js
@@ -0,0 +1,17 @@
+$('.carousel-porque-estudar').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    /*prevArrow: '.arrow_prev',
+    nextArrow: '.arrow_next',*/
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }]
+});
