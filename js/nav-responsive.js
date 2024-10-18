2e962b0d8ba6cc5c41337bb8f108542ed9da68d7
Bruno Gazola de Paoli <bpgazola@hotmail.com>
2024-10-18 10:20:03 -0300

    Delete js directory

--- js/nav-responsive.js
+++
@@ -1,7 +0,0 @@
-// SCRIPT PARA O MENU RESPONSIVO
-
-$(document).ready(function() {
-    $('.menu-trigger').click(function(){
-  $("nav ul li").toggle();
-    });
-});
--- js/remove-active-nav.js
+++
@@ -1,8 +0,0 @@
-// SCRIPT PARA RETIRAR O ACTIVE DO MENU
-
-$(document).ready(function() {
-      $("nav a").on("click", function(event){
-      $("nav").find(".active").removeClass("active");
-      $(this).parent().addClass("active");
-   });
-});
